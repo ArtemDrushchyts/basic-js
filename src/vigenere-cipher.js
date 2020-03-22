377f6d1ee076c9f081bafe168d7e162feecaacc2
Artem Drushchyts <temafm-3@yandex.ru>
2020-03-22 22:24:13 +0300

    feat: complete vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,47 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    constructor( direct = true){
+        this.direct = direct;
+        this.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        return this;
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    encrypt(message, key) {
+        if(!message || !key) {
+           throw 'Not implemented';
+        }
+        
+        let encryptMessage = '';
+    
+        for(let i = 0, j = 0; i < message.length; i++){
+            if(/[A-Za-z]/.test(message[i]) == true){
+                encryptMessage += this.alphabet[(this.alphabet.indexOf(message[i].toUpperCase()) + this.alphabet.indexOf(key[j%key.length].toUpperCase())) % this.alphabet.length];
+                j++;
+            }else{
+                encryptMessage += message[i];
+            }
+        }
+        return this.direct? encryptMessage: encryptMessage.split('').reverse().join('');
     }
+
+    decrypt(encryptedMessage, key) {
+
+        if(!encryptedMessage || !key){
+            throw 'Not implemented';
+        }
+
+        let message = '';
+    
+        for(let i = 0, j = 0; i < encryptedMessage.length; i++){
+            if(/[A-Za-z]/.test(encryptedMessage[i]) == true){
+                message += this.alphabet[(this.alphabet.length - this.alphabet.indexOf(key[j%key.length].toUpperCase()) + this.alphabet.indexOf(encryptedMessage[i].toUpperCase()))%this.alphabet.length];
+                j++;
+            }else{
+               message += encryptedMessage[i];
+            }
+        }
+        return this.direct ? message: message.split('').reverse().join(''); 
+    }
+    
 }
 
 module.exports = VigenereCipheringMachine;
